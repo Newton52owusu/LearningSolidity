2f0a95e3ee7f60856d64384b5a2ba4787ca0b0bf
Newton52owusu <newtonowusu14@gmail.com>
2023-06-05 06:15:32 -0700

    Update

--- WEB_3_SIMPLE_STORAGE/deploy.py
+++ WEB_3_SIMPLE_STORAGE/deploy.py
@@ -1,12 +1,60 @@
 from solcx import compile_standard
+import json
+from web3 import Web3
+#from solcx import install_solc
+#install_solc("0.6.0")
 
+#Read solidity file
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+    #print(simple_storage_file)
     
-    
+# Compile our solidty    
 compiled_sol = compile_standard(
+    {
     "language": "Solidity",
     "sources":{"SimpleStorage.sol": {"content": simple_storage_file}},
-    
+    "settings": {
+        "outputSelection": {
+            "*"  : {
+                "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+            }
+        }
+    },
+    },
+    solc_version="0.6.0",
+
 )
+#print(compiled_sol)
+#Compile to json
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file) 
+    
+# get bytecode //convert in python
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+
+#for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+chain_id = 5777
+my_address = "0x0f7D2779B436Fd5CB1489ed207944560E98882C7"
+priave_key = "0xb76f6e987c1d64c858e694f72b2127a0f06055af4b7baf283c42a06fad3a928f"
+
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+#print(SimpleStorage)
+ 
+# Get the latest transaction
+nonce = w3.eth.get_transaction_count(my_address)
+print(nonce) 
+
+# 1. Build transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+transaction = SimpleStorage.constructor().build_transaction({"chainid":chain_id, "from":my_address, "nonce":nonce})
+print(transaction)
